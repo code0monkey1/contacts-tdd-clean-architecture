df0198ca9aeb9e6e2d08f2891f57dff47f620c98
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-21 13:12:09 +0000

    21 Jan 2024, 18:42

---
+++ src/presentation/routers/contact/contact-router.ts
@@ -0,0 +1,7 @@
+import { CreateContactUseCase } from "../../../domain/interfaces/use-cases/contact/crate-contact-use-case";
+
+
+export default function ContactsRouter(createContactUseCase:CreateContactUseCase){
+
+
+}
