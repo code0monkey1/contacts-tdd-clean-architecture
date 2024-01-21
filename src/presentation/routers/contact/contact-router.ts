22fe6d4d982f237335c27c83bb5fba5c8f5b24a6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-21 13:14:01 +0000

    21 Jan 2024, 18:44

--- src/presentation/routers/contact/contact-router.ts
+++ src/presentation/routers/contact/contact-router.ts
@@ -1,7 +1,8 @@
+import express from 'express';
 import { CreateContactUseCase } from "../../../domain/interfaces/use-cases/contact/crate-contact-use-case";
 
-
 export default function ContactsRouter(createContactUseCase:CreateContactUseCase){
-
-
+   
+  const router = express.Router()
+  
 }
