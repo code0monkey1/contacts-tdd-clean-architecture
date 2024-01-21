f3cb87e6b38c0fb31d3d29f6f6598940876ef618
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-21 08:46:05 +0000

    21 Jan 2024, 14:16

--- src/data/interfaces/data-sources/contact-data-source.ts
+++ src/data/interfaces/data-sources/contact-data-source.ts
@@ -1,4 +1,13 @@
+import { Contact } from "../../../domain/entities/Contact";
+import { ContactResponseModel } from "../../../domain/models";
+import { ContactUpdateModel } from '../../../domain/models/index';
+
 export interface ContactDataSource {
      
-  
+      create(contact:Contact):Promise<boolean>
+      getOne(id:string):Promise<ContactResponseModel>
+      getAll():Promise<ContactResponseModel[]>
+      deleteOne(id:string):Promise<boolean>
+      updateOne(id:string,data:ContactUpdateModel):Promise<boolean>
+
 }
