c48db6ca65b8db0e7d3a330b75169f1ea6b4c049
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-21 13:34:42 +0000

    21 Jan 2024, 19:04

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
+import mongoose from "mongoose";
+import { MongodbContactDataSource } from "./data/data-sources/contact/no-sql/mongodb-contact-data-source";
+import { NoSqlDbWrapper } from "./data/interfaces/db-wrappers/nosql-db-wrapper";
 import ContactRouter from "./presentation/routers/contact/contact-router";
 import server from "./server";
 
+async function getMongoDs(){
+
+    const contactDb:NoSqlDbWrapper={
+      
+      find: function (id: string): Promise<any[]> {
+        throw new Error("Function not implemented.");
+      },
+      insertOne: function (data: any): Promise<any> {
+        throw new Error("Function not implemented.");
+      },
+      deleteOne: function (id: string): Promise<any> {
+        throw new Error("Function not implemented.");
+      },
+      updateOne: function (id: string, data: any): Promise<any> {
+        throw new Error("Function not implemented.");
+      }
+    
+    }
+    
+    return new MongodbContactDataSource(contactDb)
+}
 
 
