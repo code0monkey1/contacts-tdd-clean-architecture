065f1ae90f2256050543a40a42d211a377d4b793
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-23 14:16:11 +0000

    23 Jan 2024, 19:46

--- src/data/interfaces/db-wrappers/nosql-db-wrapper.ts
+++ src/data/interfaces/db-wrappers/nosql-db-wrapper.ts
@@ -1,6 +1,7 @@
 export interface NoSqlDbWrapper{
-  find(id:string):Promise<any[]>
-  insertOne(data:any):Promise<any|null>
+  find(query:object):Promise<any[]>
+  findOne(id:string):Promise<any|null>
+  insertOne(data:object):Promise<any|null>
   deleteOne(id:string):Promise<any>
   updateOne(id:string,data:any):Promise<any>
   
