e53c7a6f96926c426cffcafc1122a392bab68751
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-23 15:22:00 +0000

    23 Jan 2024, 20:52

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,52 @@
-import mongoose from "mongoose";
+
 import { MongodbContactDataSource } from "./data/data-sources/contact/no-sql/mongodb-contact-data-source";
 import { NoSqlDbWrapper } from "./data/interfaces/db-wrappers/nosql-db-wrapper";
+import { ContactRepository } from './domain/interfaces/repositories/contacts/contact-repository';
+import { ContactRepositoryImpl } from './domain/repositories/contact/contact-repository-impl';
+import { CreateContact } from "./domain/use-cases/create-contact";
 import ContactRouter from "./presentation/routers/contact/contact-router";
 import server from "./server";
 
-async function getMongoDs(){
+import { ContactDataSource } from "./data/interfaces/data-sources/contact/contact-data-source";
+import ContactModel from "./data/schemas/contact/contact-model";
+
+async function getMongoDs() :Promise<ContactDataSource>{
 
     const contactDb:NoSqlDbWrapper={
       
-      find: function (id: string): Promise<any[]> {
-        throw new Error("Function not implemented.");
+      find: async function (query: object): Promise<any[]> {
+        const contacts = await ContactModel.find(query);
+        return contacts;
       },
-      insertOne: function (data: any): Promise<any> {
-        throw new Error("Function not implemented.");
+      findOne: async function (id: string): Promise<any> {
+        const contact = await ContactModel.findOne({ _id: id });
+        return contact;
       },
-      deleteOne: function (id: string): Promise<any> {
-        throw new Error("Function not implemented.");
+      insertOne: async function (data: object): Promise<any> {
+        const contact = await ContactModel.create(data);
+
+        return contact;
       },
-      updateOne: function (id: string, data: any): Promise<any> {
-        throw new Error("Function not implemented.");
-      }
+      updateOne: async function (id: string, data: object): Promise<any> {
+        const contact = await ContactModel.findOneAndUpdate({ _id: id }, data, {
+          new: true,
+        });
+        return contact ;
+      },
+      deleteOne: async function (id: string): Promise<void> {
+        await ContactModel.deleteOne({ _id: id });
+      },
     
     }
-    
+                 
     return new MongodbContactDataSource(contactDb)
 }
 
 (async ()=>{
+
+  const dataSource =await getMongoDs()
+
+  const contactsRouter=ContactRouter( new CreateContact( new ContactRepositoryImpl(dataSource)))
 
   server.listen(3000,()=>{
   
