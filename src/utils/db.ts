7838e5037debd638981ab1caa553f7eeeb90c5e1
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-23 12:38:05 +0000

    23 Jan 2024, 18:08

---
+++ src/utils/db.ts
@@ -0,0 +1,28 @@
+import * as dotenv from 'dotenv';
+import mongoose, { ConnectOptions } from 'mongoose';
+import utils from '../utils';
+
+dotenv.config();
+export default class Database {
+  private static _database: Database;
+  private constructor() {
+    const DB_URL = utils.MONGO_DB_URI;
+
+    if (DB_URL) {
+      mongoose
+        .connect(DB_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        } as ConnectOptions)
+        .then(() => console.log('✅ Connected with database'))
+        .catch(() => console.error('❌ Not connected with database'));
+    }
+  }
+  static connect() {
+    if (this._database) {
+      return this._database;
+    }
+    this._database = new Database();
+    return (this._database = new Database());
+  }
+}
