e692346794821bc41c921a9fea17401b56fb75fb
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-01-24 07:53:14 +0000

    24 Jan 2024, 13:23

---
+++ src/infra/http/middlewares/Authentication/AuthMiddleware.ts
@@ -0,0 +1,10 @@
+import { HttpRequest } from "../../interfaces/HttpRequest";
+import { HttpResponse } from "../../interfaces/HttpResponse";
+import { BaseMiddleware } from "../BaseMiddleware";
+
+export class AuthMiddleware extends BaseMiddleware{
+  execute(httpRequest: HttpRequest): Promise<HttpResponse> {
+    throw new Error("Method not implemented.");
+  }
+  
+}
