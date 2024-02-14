01ca7d40b80a33d25b90ed8a7301c2bea7e7ea69
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-02-14 13:21:19 +0000

    14 Feb 2024, 18:51

--- src/infra/http/validations/EmailValidation.ts
+++ src/infra/http/validations/EmailValidation.ts
@@ -1,9 +1,13 @@
-
+import { z } from 'zod';
 import { EmailValidator } from "./interfaces/EmailValidator";
 
-export class EmailValidation implements EmailValidator{
-  isValid: (email: string) => boolean;
+const emailSchema = z.string().email();
+
+export class EmailValidation implements EmailValidator {
+  isValid(email: string): boolean {
+
+    const parsedEmail = emailSchema.safeParse(email);
+    return parsedEmail.success;
+  }
   
-
-
 }
