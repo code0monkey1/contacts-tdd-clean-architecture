b22d4e8ad3922abc4cb84dd3eed1b5d181f4f97d
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-03-14 19:13:41 +0000

    15 Mar 2024, 00:43

--- src/presentation/routers/contact/contact-router.ts
+++ src/presentation/routers/contact/contact-router.ts
@@ -1,74 +1,60 @@
-import express, { Request, Response } from 'express';
-import { Contact } from '../../../domain/entities/Contact';
-import { ContactResult } from '../../../domain/entities/ContactResult';
+import express, { Request, Response } from "express";
+import { Contact } from "../../../domain/entities/Contact";
+import { ContactResult } from "../../../domain/entities/ContactResult";
 import { CreateContactUseCase } from "../../../domain/interfaces/use-cases/contact/crate-contact-use-case";
-import { GetContactUseCase } from '../../../domain/interfaces/use-cases/contact/get-contact-use-case';
+import { GetContactUseCase } from "../../../domain/interfaces/use-cases/contact/get-contact-use-case";
 
 export default function ContactRouter(
   createContactUseCase: CreateContactUseCase,
   getContactUseCase: GetContactUseCase
 ) {
+  const router = express.Router();
 
-  const router = express.Router()
-
-
-  router.post('/', async (req: Request, res: Response) => {
-
+  router.post("/", async (req: Request, res: Response) => {
     try {
+      const contactResult: ContactResult = await createContactUseCase.execute(
+        req.body
+      );
 
-      const contactResult: ContactResult = await createContactUseCase.execute(req.body)
+      console.log(" ✅ New Contact Created");
 
-      console.log(" ✅ New Contact Created")
-
-      res.json(contactResult)
-
+      res.json(contactResult);
     } catch (err) {
-
-      let message = ''
+      let message = "";
 
       if (err instanceof Error) {
-        message += err.message
+        message += err.message;
       }
-      console.log("Contact Created :no_entry_sign:")
-      res.status(500).json({ error: `error fetching data : ${message}` })
-
+      console.log("Contact Created :no_entry_sign:");
+      res.status(500).json({ error: `error fetching data : ${message}` });
     }
+  });
 
-  })
-
-  router.get('/:id', async (req: Request, res: Response) => {
-
+  router.get("/:id", async (req: Request, res: Response) => {
     try {
+      const { id } = req.params;
 
-      const { id } = req.params
+      console.log("The id is", id);
 
-      console.log("The id is", id)
+      if (!id) throw "id was not given : " + id;
 
-      if (!id)
-        throw ("id was not given : " + id)
+      const contact = await getContactUseCase.execute(id);
 
-      const contact = await getContactUseCase.execute(id)
+      console.log(" ✅  Contact Found", JSON.stringify(contact));
 
-      console.log(" ✅  Contact Found", JSON.stringify(contact))
-
-      res.json(contact)
-
+      res.json(contact);
     } catch (err) {
-
-      let message = ''
+      let message = "";
 
       if (err instanceof Error) {
-        message += err.message
+        message += err.message;
       }
 
-      console.log(" ❌ Contact not found ")
+      console.log(" ❌ Contact not found ");
 
-      res.status(500).json({ error: `Error getting contact : ${message}` })
-
+      res.status(500).json({ error: `Error getting contact : ${message}` });
     }
+  });
 
-  })
-
-  return router
-
+  return router;
 }
